ae7f00f29e02c8b56bf30b4e659839de9e9562f7
LucassAlbu <lucassilva.alb@hotmail.com>
2023-01-18 00:03:57 -0300

    Feat: criação do layout da tela de detalhes

--- app/src/main/java/com/wip/tech/visitcroatia/ui/fragments/AttractionDetailsFragment.kt
+++ app/src/main/java/com/wip/tech/visitcroatia/ui/fragments/AttractionDetailsFragment.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
+import com.wip.tech.visitcroatia.data.Attraction
 import com.wip.tech.visitcroatia.databinding.FragmentAttractionDetailsBinding
 
-class AttractionDetailsFragment : Fragment() {
+class AttractionDetailsFragment : BaseFragment() {
 
     private var _binding: FragmentAttractionDetailsBinding? = null
     private val binding get() = _binding!!
 
     private val safeArgs: AttractionDetailsFragmentArgs by navArgs()
+
+    private val attraction: Attraction by lazy {
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +31,15 @@ class AttractionDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button.text = safeArgs.attractionId
+        binding.titleTextView.text = attraction.title
+        binding.descriptionTextView.text = attraction.description
+        Picasso.get().load(attraction.image_urls).into(binding.headerImageView)
+        binding.monthsToVisitTextView.text = attraction.months_to_visit
+        binding.numberOfFactsTextView.text = "${attraction.facts.size}facts"
+        binding.numberOfFactsTextView.setOnClickListener{
+            //TODO
+        }
+
     }
 
     override fun onDestroyView() {
